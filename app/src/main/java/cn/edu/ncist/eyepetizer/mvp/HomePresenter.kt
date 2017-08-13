4dd01489c1cb3279433b2d50b08b0b4bce6af4e5
fazhongxu <fazhongxu0914@163.com>
2017-08-13 17:38:34 +0800

    添加HomePresenter

--- app/src/main/java/cn/edu/ncist/eyepetizer/mvp/HomePresenter.kt
+++ app/src/main/java/cn/edu/ncist/eyepetizer/mvp/HomePresenter.kt
@@ -1,12 +1,33 @@
 package cn.edu.ncist.eyepetizer.mvp
 
+import android.content.Context
 import cn.edu.ncist.eyepetizer.bean.HomeBean
 import cn.edu.ncist.eyepetizer.mvp.contract.HomeContract
+import cn.edu.ncist.eyepetizer.mvp.model.HomeModel
+import java.util.*
 
 /**
  * Created by xxl on 2017/7/24.
  */
-class HomePresenter : HomeContract.View, HomeContract.Presenter {
+class HomePresenter constructor(var context: Context) : HomeContract.View, HomeContract.Presenter {
+
+    val mHomeModel : HomeModel by lazy{  //lazy模式，对象要在真正调用的地方才实例化 lazy模式值为val
+        HomeModel()
+    }
+
+
+//    var mContext : Context? = null
+
+    init {
+//        mContext = context
+//        var callback : (()->Unit)? = null   //无参数回调
+//        callback?.invoke()
+
+        var callback : ((str : String) -> Unit)? = null  //有参数回调
+
+        callback?.invoke("aaa")
+
+    }
 
     override fun start() {
 
@@ -17,6 +38,8 @@ class HomePresenter : HomeContract.View, HomeContract.Presenter {
     }
 
     override fun requestData() {
-    }
+        val subscribe = mHomeModel.loadData(context, true, "0")?.subscribe {
 
+        }
+    }
 }
--- app/src/main/java/cn/edu/ncist/eyepetizer/mvp/model/HomeModel.kt
+++ app/src/main/java/cn/edu/ncist/eyepetizer/mvp/model/HomeModel.kt
@@ -1,8 +1,23 @@
 package cn.edu.ncist.eyepetizer.mvp.model
 
+import android.content.Context
+import cn.edu.ncist.eyepetizer.bean.HomeBean
+import cn.edu.ncist.eyepetizer.network.ApiService
+import cn.edu.ncist.eyepetizer.network.RetrofitClient
+import cn.edu.ncist.eyepetizer.utils.applySchedulers
+import io.reactivex.Observable
+
 /**
  * Created by xxl on 2017/7/24.
  */
 class HomeModel {
+    fun loadData(context: Context, isFirstLoad: Boolean,data: String) : Observable<HomeBean>? {
+        val retrofit = RetrofitClient.getInstance(context, ApiService.BASE_URL)
+        val apiService = retrofit?.create(ApiService::class.java)
+        when(isFirstLoad) {
+            true -> return apiService?.getHomeData()
 
+            false -> return apiService?.getHomeMoreData(data,"2")?.applySchedulers()
+        }
+    }
 }
